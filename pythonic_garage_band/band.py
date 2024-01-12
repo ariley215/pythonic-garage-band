21abe329dbcec5b805a16544a405643ec5b0f67b
Andrea Riley <andreariley@me.com>
2024-01-11 20:23:33 -0500

    guitarist_str test passing

---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,8 @@
+class Guitarist():
+    
+    def __init__(self, name="Joan Jett"):
+       self.name = name
+       
+
+    def __str__(self):
+        return f'My name is {self.name} and I play guitar'
