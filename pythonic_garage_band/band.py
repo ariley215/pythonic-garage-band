a666f4e1cf538e1588068f12e510e1955a34f686
Andrea Riley <andreariley@me.com>
2024-01-11 21:19:41 -0500

    sheli e tests passed

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -9,4 +9,18 @@ class Guitarist():
         return f'My name is {self.name} and I play guitar'
 
     def __repr__(self):
-        return "Guitarist instance. Name = Joan Jett"
+        return f"Guitarist instance. Name = {self.name}"
+    
+
+class Drummer():
+       
+    def __init__(self, name="Sheila E."):
+       self.name = name
+      
+    
+    def __str__(self):
+        return f'My name is {self.name} and I play drums'
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+    
